f3fc3d53def4c6486446e5b5d81a637192293ae0
kevin <righttrianglesrkewl3@gmail.com>
2021-12-19 14:12:32 -0800

    working to pull load test images

--- backend/app/client.py
+++ backend/app/client.py
@@ -6,20 +6,20 @@ import os
 import time
 from imutils import paths
 
-HERE = Path(__file__).parent.absolute()
+# HERE = Path(__file__).parent.absolute()
+# print(f'HERE: {HERE}')
 
 def imlist(path):
-    """
-    The function imlist returns all the names of the files in 
-    the directory path supplied as argument to the function.
-    """
     return [os.path.join(path, f) for f in os.listdir(path)]
 
-paths = imlist("/home/batman/Desktop/fast_api_image_validator/docs")
+# path to local folder with load test imgs
+paths = imlist(os.path.join("api", "load_test_imgs"))
 
+
+print(paths)
 for idx, path in enumerate(paths):
     # if idx <= 3:
-    with open(HERE / f"{path}", "rb") as fh:
+    with open(path, "rb") as fh:
         url = "http://localhost:8000/api/isvalid/validate_image"
         files = {"upload_file": fh}
         values = {
